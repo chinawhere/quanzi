75cd7930c23e0b5e9600b79d7ffcdaa0b0bfd4ba
lizhenjiang <hellolzj123@163.com>
2020-03-01 00:44:27 +0800

    deploy

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -6,7 +6,7 @@ WxPay.mch_id = ENV['WX_MCH_ID']
 # cert, see https://pay.weixin.qq.com/wiki/doc/api/app.php?chapter=4_3
 # using PCKS12
 # WxPay.set_apiclient_by_pkcs12(File.binread("#{ENV['WX_PKCS12_FILEPATH']}"), ENV['WX_API_KEY'])
-WxPay.set_apiclient_by_pkcs12(File.binread("#{ENV['WX_PKCS12_FILEPATH']}"), ENV['WX_MCH_ID'])
+# WxPay.set_apiclient_by_pkcs12(File.binread("#{ENV['WX_PKCS12_FILEPATH']}"), ENV['WX_MCH_ID'])
 
 # optional - configurations for RestClient timeout, etc.
 WxPay.extra_rest_client_options = {timeout: 5, open_timeout: 8}
